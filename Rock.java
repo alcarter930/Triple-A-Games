91dd62cdfe697530c70eaae2f093e777eaae91af
amullapudi1 <112345845+amullapudi1@users.noreply.github.com>
2022-09-17 17:08:50 -0400

    Update Rock.java

--- Rock.java
+++ Rock.java
@@ -33,6 +33,7 @@ public class Rock{
     group.add(paperButton);
 
     JButton submit =new JButton("Submit");
+    JButton submit2 = new JButton("Submit");
 
     private String playerChoice = "";
     private String playerChoice2 = "";
@@ -44,39 +45,55 @@ public class Rock{
     group2.add(rockButton2);
     group2.add(scissorsButton2);
     group2.add(paperButton2);
-    public String getWinner(){
-        String winner = "";
-        if(playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("rock")){
-            winner = "tie";
-        }
-        else if(playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("scissors")){
-            winner = "tie";
-        }
-        else if(playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("paper")){
-            winner = "tie";
-        }
-        else if(playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("scissors")){
-            winner = player1;
-        }
-        else if(playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("paper")){
-            winner = player2;
-        }
-        else if(playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("rock")){
-            winner = player2;
-        }
-        else if(playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("paper")){
-            winner = player1;
-        }
-        else if(playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("rock")){
-            winner = player1;
-        }
-        else if(playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("scissors")){
-            winner = player2;
-        }
-        return winner;
-    }
 
+    //ends game, announces winner
+    submit2.addActionListener(new ActionListener() {
+        public String getWinner () {
+            String winner = "";
+            if (playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("rock")) {
+                winner = "tie";
+            } else if (playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("scissors")) {
+                winner = "tie";
+            } else if (playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("paper")) {
+                winner = "tie";
+            } else if (playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("scissors")) {
+                winner = player1;
+            } else if (playerChoice.equalsIgnoreCase("rock") && playerChoice2.equalsIgnoreCase("paper")) {
+                winner = player2;
+            } else if (playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("rock")) {
+                winner = player2;
+            } else if (playerChoice.equalsIgnoreCase("scissors") && playerChoice2.equalsIgnoreCase("paper")) {
+                winner = player1;
+            } else if (playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("rock")) {
+                winner = player1;
+            } else if (playerChoice.equalsIgnoreCase("paper") && playerChoice2.equalsIgnoreCase("scissors")) {
+                winner = player2;
+            }
+            return winner;
+        }
+    });
+
+    //player 2 choosing
     public void turn2(){
+        JFrame frame1 = new JFrame("Rock-Paper-Scissors");
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Please Make Your Selection: ");
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10,10,10,10);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(label);
+        c.gridy++;
+        panel.add(rockButton2);
+        c.gridy++;
+        panel.add(scissorsButton2);
+        c.gridy++;
+        panel.add(paperButton2);
+        c.gridy+=4;
+        panel.add(submit2);
+        frame1.add(panel);
         if(rockButton2.isSelected()){
             playerChoice2 = "rock";
             getWinner();
@@ -87,13 +104,34 @@ public class Rock{
         }
         else if(paperButton2.isSelected()){
             playerChoice2 = "paper";
+            getWinner();
         }
         else{
             JOptionPane.showMessageDialog(null, "Please select an option!");
         }
     }
 
+    //player 1 choosing
     submit.addActionListener(new ActionListener(){
+        JFrame frame2 = new JFrame("Rock-Paper-Scissors");
+        JPanel panel2 = new JPanel();
+        JLabel label2 = new JLabel("Please Make Your Selection: ");
+        panel2.setLayout(new GridBagLayout());
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.insets = new Insets(10,10,10,10);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        c1.gridy = 0;
+        panel2.add(label2);
+        c1.gridy++;
+        panel2.add(rockButton);
+        c1.gridy++;
+        panel2.add(scissorsButton);
+        c1.gridy++;
+        panel2.add(paperButton);
+        c1.gridy+=4;
+        panel2.add(submit);
+        frame2.add(panel2);
         if(rockButton.isSelected()){
             playerChoice = "rock";
             turn2();
@@ -104,7 +142,7 @@ public class Rock{
         }
         else if(paperButton.isSelected()){
             playerChoice = "paper";
-            turn2()
+            turn2();
         }
         else{
             JOptionPane.showMessageDialog(null, "Please select an option!");
