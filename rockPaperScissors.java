cc06bdd88a27a18defbe3b977ba4d833244c53fe
amullapudi1 <112345845+amullapudi1@users.noreply.github.com>
2022-09-17 10:40:09 -0400

    Create rockPaperScissors.java

---
+++ rockPaperScissors.java
@@ -0,0 +1,16 @@
+public class Rock{
+  
+  private String player1;
+  private String player2;
+  
+  public Rock(name1, name2){
+   this.player1 = name1;
+   this.player2 = name2;
+  }
+  
+  public String getWinner(){
+    
+  }
+  
+  
+}
