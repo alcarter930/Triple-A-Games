83dfcc1d8dfcc7f906bce3393e038b0c50bbddb3
alcarter930 <112433633+alcarter930@users.noreply.github.com>
2022-09-17 12:09:33 -0400

    Made the GameSelector class
    introduced two constructors for the GameSelector class. implementation is described in comments

---
+++ GameSelector.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+public class GameSelector{
+  
+  private String[] P1Games, P2Games;
+  public final String[] P1GAMES = {"Trivia"};
+  public final String[] P2GAMES = {"Rock, Paper, Scissors", "Trivia"};
+  protected int[] scores;
+  
+  /*
+  constructor
+  takes arrayList of player names as parameter
+  */
+  public GameSelector(ArrayList<String> names){
+
+    scores = new int[names.size()];
+    for(int i = 0; i<scores.length; i++){
+      scores[i] = 0;
+    }
+  }
+  /*
+  constructor
+  takes arraylist of names as parameter and array of scores
+  */
+  public GameSelector(ArrayList<String> names, int[] orgScore){
+    scores = orgScore;
+
+  }
+  
+}
